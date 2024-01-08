61f4ff09392d94dfe4d0e28d1807e06a72806a28
Lourdes Infante <117685514+LourInf@users.noreply.github.com>
2024-01-08 18:28:52 +0000

    install and configure flask-jwt-extended

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,16 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/login", methods=["POST"])   #we change app for api #note: this won't be visible in the browser as it's POST (we use postman for that) In the browser we see only GET requests
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    # here we set the logic to consult if user exists in our DB
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)  #remember to import create_access_token!
+    return jsonify(access_token=access_token)
